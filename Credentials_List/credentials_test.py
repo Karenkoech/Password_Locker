3d88241e76bbec535b00dd65b43fceff96acfa43
Karenkoech <karenkoech3@gmail.com>
2022-04-24 11:26:08 -0400

    write the tests that will be run to see the behaviour of the app

--- Credentials_List/credentials_test.py
+++ Credentials_List/credentials_test.py
@@ -1,5 +1,6 @@
 import unittest # Importing the unittest module
 from credentials import Credentials #Importing the credentials class
+import pyperclip
 
 class TestCredentials(unittest.TestCase):
     """
@@ -12,6 +13,7 @@ class TestCredentials(unittest.TestCase):
         Set up method to run before each test cases.
         """
         self.new_credentials = Credentials("karen" "karen123","karen@gmail") # create credentials object
+
 
     def test_init(self):
         """
@@ -28,6 +30,55 @@ def test_save_credentials(self):
     """
     self.new_credentials.save_credentials() # saving the new credentials
     self.assertEqual(len(Credentials.credentials_list),1)
+
+def tearDown(self):
+    """
+    tearDown method that does clean up after each test case has run.
+    """
+    Credentials.credentials_list = []
+
+def test_save_multiple_credentials(self):
+    """
+    test_save_multiple_credentials to check if we can save multiple credentials
+    objects to our credentials_list
+    """
+    self.new_credentials.save_credentials()
+    test_credentials = Credentials("karen","karen123","karen@gmail") # new credentials
+    test_credentials.save_credentials()
+    self.assertEqual(len(Credentials.credentials_list),2)
+
+def test_delete_credentials(self):
+    """ 
+    test_delete_credentials to test if we can remove a credentials from our credentials list"""  
+    self.new_credentials.save_credentials()
+    test_credentials = Credentials("karen","karen123","karen@gmail") # new credentials
+    test_credentials.save_credentials()
+
+    self.new_credentials.delete_credentials() # Deleting a credentials object
+    self.assertEqual(len(Credentials.credentials_list),1)
+
+def  test_find_credentials_by_name(self):
+    """
+    test to check if we can find a credentials by account name and display information """
+    
+    self.new_credentials.save_credentials()
+    test_credentials = Credentials("karen","karen123","karen@gmail") # new credentials
+    test_credentials.save_credentials()
+
+    found_credentials = Credentials.find_by_name("karen")
+
+    self.assertEqual(found_credentials.account_name,test_credentials.account_name)
+
+def test_copy_email_(self):
+    """
+    Test to confirm that we are copying the email address from a found credentials.
+    """
+    self.new_credentials.save_credentials()
+    Credentials.copy_email("karen")
+
+    self.assertEqual(self.new_credentials.account_email,pyperclip.paste())
+
+
 
 
 
